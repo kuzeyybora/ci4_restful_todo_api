59aed95701c79aa2b4ed473cee881b82f7f09e18
Kuzey Bora <kuzeybora01@hotmail.com>
2024-11-23 06:31:24 +0300

    Some errors and mistakes in the LanguageFilter were fixed.

--- app/Filters/LanguageFilter.php
+++ app/Filters/LanguageFilter.php
@@ -9,20 +9,17 @@ class LanguageFilter implements FilterInterface
 {
     /**
      * This function sets the application's language locale based on the userLanguage request header.
-     * If userLanguage is one of the allowed languages (defined in LanguageKeys::ALLOWED_LOCALS),
      * it uses that locale; otherwise, it defaults to Turkish (tr). The locale is then set via the language service.
      * */
-    public function before(RequestInterface $request, $arguments = null): ?ResponseInterface
+    public function before(RequestInterface $request, $arguments = null): void
     {
         $userLanguage = $request->getHeaderLine('userLanguage') ?? 'tr';
 
-        $locale = in_array($userLanguage, config(App::class)->supportedLocales)
+        $locale = in_array($userLanguage, config("app")->supportedLocales)
             ? $request->getHeaderLine('userLanguage')
             : 'tr';
 
         service('language')->setLocale($locale);
-
-        return null;
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null){}

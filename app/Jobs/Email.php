dca9d560308cc0fabee51123f68977d7979ec1b0
Kuzey Bora <kuzeybora01@hotmail.com>
2025-01-04 08:22:40 +0300

    A queue system was integrated into the project and configured using the Redis handler. Additionally, an email job file was created.

---
+++ app/Jobs/Email.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use CodeIgniter\Queue\BaseJob;
+use CodeIgniter\Queue\Interfaces\JobInterface;
+
+class Email extends BaseJob implements JobInterface
+{
+    public function process(): bool
+    {
+        // Job verilerini kontrol edelim
+        log_message('info', 'Email job data: ' . json_encode($this->data));
+
+        $emailService = service('email', null, false);
+
+        $result = $emailService
+            ->setTo($this->data['email'])
+            ->setSubject($this->data['subject'])
+            ->setMessage($this->data['message'])
+            ->send(false);
+
+        if (! $result) {
+            log_message('error', 'Email failed: ' . $emailService->printDebugger('headers'));
+            throw new \Exception($emailService->printDebugger('headers'));
+        }
+
+        log_message('info', 'Email sent successfully to: ' . $this->data['email']);
+
+        return true;
+    }
+}
